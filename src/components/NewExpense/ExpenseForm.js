9c2b5f1547f399145241b1974fffb2f316ed1f56
sakshampatil <69973154+sakshampatil@users.noreply.github.com>
2021-10-20 23:37:10 +0530

    done

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -61,14 +61,12 @@ const ExpenseForm = (props) => {
             onChange={dateChangeHandler}
           />
         </div>
-        <div className="new-expense__control">
-          <button type="submit">Add Expense</button>
-        </div>
-        <div>
-          <button type="button" onClick={props.onCancel}>
-            Cancel
-          </button>
-        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
